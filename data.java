92552ac33a2e40a959390129bddd1515084ee225
ansarizoya13 <ansari.zoyaa13@gmail.com>
2024-01-29 14:19:06 +0530

    data

---
+++ data.java
@@ -0,0 +1,23 @@
+class data
+{
+	public static void main(String args[])
+	{
+		int num1=20;
+		float num2=20.50f;
+		char letter='Z';
+		String words="Hello World";
+		boolean check=true;
+		long num3=9l;
+		double num4=0.50d;
+		byte num5=2;
+		
+		System.out.println("INTEGER IS "+num1);
+		System.out.println("THE FLOAT NUM IS "+num2);
+		System.out.println("THE LETTER IS "+letter);
+		System.out.println("THE STRING IS "+words);
+		System.out.println("THE VALUE IS "+check);
+		System.out.println("THE LONG NUM IS "+num3);
+		System.out.println("THE DOUBLE NUM IS "+num4);
+		System.out.println("THE BYTE NUM IS "+num5);
+	} 
+}
