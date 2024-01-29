3630210e8c8729b4f14d044937485b331fddf19f
ansarizoya13 <ansari.zoyaa13@gmail.com>
2024-01-29 14:41:57 +0530

    Squareroot

---
+++ Squareroot.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class Squareroot
+{
+ 	public static void main(String args[])
+	{
+	    double x = 5;
+	    double y;
+	    y = Math.sqrt(x);
+	    System.out.println("y = "+y);
+	}
+}
