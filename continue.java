a4dc0ce522b8b8ca6920472303ffe40960e760fd
ansarizoya13 <ansari.zoyaa13@gmail.com>
2024-01-29 14:19:58 +0530

    continue

---
+++ continue.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class lineuser
+{
+	public static void main(String args[0])
+	   {
+		short num1;
+		Scanner scin = new Scanner(System.in);
+	        System.out.println("Enter the number");
+	        num1 = scin.nextShort();
+	        for(int i =1;i<=10;++i)
+	           {
+		     if(num1>=1 && num<=10)
+			{
+			  continue;
+			}
+		     else
+			{
+			System.out.println("the natural numbers are +i");
+			}	
+		   }
+ 
+	   }
+}
