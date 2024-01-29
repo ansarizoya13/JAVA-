77945ba29b9c1af1635e6080696de288dbf8aa7f
ansarizoya13 <ansari.zoyaa13@gmail.com>
2024-01-29 14:41:28 +0530

    minmax

---
+++ minmax.java
@@ -0,0 +1,21 @@
+class minmax
+{
+	public static void main(String args[])
+		{
+		   System.out.println("Byte is "+Byte.MAX_VALUE);
+		   System.out.println("Byte is "+Byte.MIN_VALUE);
+		   System.out.println("short is "+Short.MAX_VALUE);
+		   System.out.println("short is "+Short.MIN_VALUE);
+		   System.out.println("Long is "+ Long.MAX_VALUE);
+		   System.out.println("long is "+Long.MIN_VALUE);
+		   System.out.println("float is "+Float.MAX_VALUE);
+		   System.out.println("float is "+Float.MIN_VALUE);
+		   System.out.println("Double is "+Double.MAX_VALUE);
+		   System.out.println("Double is "+Double.MIN_VALUE);
+		//   System.out.println("INT is "+ Int.MAX_VALUE);
+		 //  System.out.println("INT is "+ Int.MIN_VALUE);
+		}
+}
+
+		   
+
