6d73673ece3685c79e6ddb55d06ef3e97fb07348
ansarizoya13 <ansari.zoyaa13@gmail.com>
2024-01-29 14:17:56 +0530

    ClassDemo

---
+++ ClassDemo2.java
@@ -0,0 +1,11 @@
+class ClassDemo2 
+{
+ 	public static void main(String args[])
+	{
+	   char ch1;
+	   ch1='X';
+	   System.out.println("ch1 contains "+ch1);
+	   ch1++;
+	   System.out.println("ch1 is now"+ch1);
+	}
+}
